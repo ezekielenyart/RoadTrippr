dbbc11501dcb3f87af5069b0937f36dbac9a1401
Nathan Szurek <nathan.szurek@yahoo.com>
2020-06-25 13:51:54 -0500

    Dynamically rendered response items to list

--- Assets/app.js
+++ Assets/app.js
@@ -66,14 +66,14 @@ $(document).ready(function () {
             // var pointLatLong = [$(this)]
             console.log(pointSelected.lat)
             
-
+            
             // Api call for nearby places.
             var stopSearch = "cafe";
             var places = {
             "async": true,
             "crossDomain": true,
             // Format of the url is [latitude, longitude]
-            "url": "https://trueway-places.p.rapidapi.com/FindPlacesNearby?type=" + stopSearch + "&radius=150&language=en&location=" + pointSelected.lat + "%252C" + pointSelected.lng,
+            "url": "https://trueway-places.p.rapidapi.com/FindPlacesNearby?type=" + stopSearch + "&radius=2000&language=en&location=" + pointSelected.lat + "%252C" + pointSelected.lng,
             // 37.783366%252C-122.402325
             "method": "GET",
             "headers": {
@@ -96,20 +96,81 @@ $(document).ready(function () {
         console.log(JSON.stringify(responsePlaces) + "Places");
         console.log("Longitude and Latitude: " + e.lngLat);
         // Stop option 1 info from api object to stop option cards
-         // select the name, address, and website of the first result from the response object
-         var stop1NameData = (responsePlaces[Object.keys(responsePlaces)[0]][0].name)
-         var stop1AddressData = (responsePlaces[Object.keys(responsePlaces)[0]][0].address)
-         var stop1WebsiteData = (responsePlaces[Object.keys(responsePlaces)[0]][0].website)
+         // select the name, address, and website of the  first result from the response object
+         for (i=0; i < 10; i++){
+         var stop1NameData = (responsePlaces[Object.keys(responsePlaces)[0]][i].name)
+         var stop1AddressData = (responsePlaces[Object.keys(responsePlaces)[0]][i].address)
+         var stop1WebsiteData = (responsePlaces[Object.keys(responsePlaces)[0]][i].website)
+         console.log("Name Data" + stop1NameData);
+
+       
           // add the text of the 1st result's name, address, and website to the stop option div
-          console.log(stop1NameData)
+        //   assign Icons based on response type
+        //  var stopIcons = "";
+
+        // TESTING 
+
+        // 
+
+        var nearbyPlaceLi = $("<li>").addClass("class filler");
+        var addStopBtn = $("<button>").addClass("btn btn-success addStopButton");
+        var placeTitle = $("<h4>").text(stop1NameData).addClass("col-12");
+        var placeAddress = $("<p>").text(stop1AddressData).addClass("col-12");
+        var placeLink = $("<a>").text("More Information");
+        placeLink.attr({"target": "_blank", "href": stop1WebsiteData});
+        addStopBtn.append(placeTitle, placeAddress);  
+        nearbyPlaceLi.append(addStopBtn, placeLink);
+        
+
+        // TESTING
+
          $("#stopOption1Name").text(stop1NameData)
          $("#stopOption1Address").text(stop1AddressData)
          $("#stopOption1Website").text(stop1WebsiteData)
-
+        //  nearbyPlaceLi.append(placeTitle, placeAddress, placeLink, addStopBtn)
+         $(".filler-list").append(nearbyPlaceLi)
         console.log(JSON.stringify(responsePlaces) + "Places");
-    });       
         console.log(e.lngLat)
+
+        }
+    
+        })
+
+    });       
+        
+    
+    var stopArray = [];
+
+    // Add Stop function for buttons on stop options
+    // when user clicks the Add Stop button...
+    $("#addStopBtn").click(function(){
+        // create a list item with the location details inside of the ol in the stop list div
+        
+        // select text from the stop option div h1
+        var stopOpt1Name = $("#stopOption1Name").text();
+        console.log("stop1 name for list: " + stopOpt1Name)
+        // add stop
+        stopArray.push("StopOpt1Name");
+        console.log(stopArray)
+
+
+            // set a variable for the id stoplist, the ordered list of saved stops
+            $("#stopList").empty();
+            var stopList = $("#stopList");
+            for (i = 0; i <= stopArray.length; i++){
+                var newStop = $("<li>").text(this.textContent);
+                stopList.append(newStop)
+            }
+    
+            console.log(this)
     });
+
+    // $("#addStopBtn").click(function(){
+    //     console.log(this);
+    //     console.log(this.textContent);
+    // });
+
+    
 
 
 
